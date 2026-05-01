0f8d2a8d17e4d22772dbfa70ba53c3ff1c59c83e
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2026-05-01 08:49:52 +0330

    push

--- src/menus/show-more.ts
+++ src/menus/show-more.ts
@@ -7,63 +7,64 @@ import { downloadMenu } from "./download";
 export const showMoreMenu = new Menu<MyContext>("show-more").text(
   "show more",
   async (ctx) => {
-    await ctx.answerCallbackQuery();
-
     const caption = ctx.update.callback_query.message?.caption;
     const href = caption?.split("\n")[2];
 
-    const message = await ctx.reply(`search for: ${href}`);
+    // ctx.answerCallbackQuery();
+    console.log("show more", { href });
 
+    const message = await ctx.reply(`search for: ${href}`);
+    const message_id = message.message_id;
     if (!href) return;
 
     ctx.session.selectedVideo = href;
 
-    const pageData = await extractPage(href);
+    show(href, message_id, ctx);
+  },
+);
 
-    if (pageData?.links?.length) {
-      ctx.session.videoDownloadLinks = pageData?.links.map(
-        (link) => link.downloadLink,
-      );
-    }
+async function show(href: string, message_id: number, ctx: MyContext) {
+  const pageData = await extractPage(href);
 
-    if (!pageData || !pageData.screenshots.length) {
-      if (ctx.chatId) {
-        return ctx.api.editMessageText(
-          ctx.chatId,
-          message.message_id,
-          "not found",
-        );
-      } else {
-        return ctx.reply("not found");
-      }
-    }
-
-    const mediaGroup: InputMediaPhoto[] = pageData.screenshots.map(
-      (screenshot) => ({
-        type: "photo",
-        media: screenshot,
-      }),
+  if (pageData?.links?.length) {
+    ctx.session.videoDownloadLinks = pageData?.links.map(
+      (link) => link.downloadLink,
     );
+  }
 
-    const linksLabel = pageData.links
-      .map((link) => `${link.quality}`)
-      .join(" | ");
-
+  if (!pageData || !pageData.screenshots.length) {
     if (ctx.chatId) {
-      await ctx.api.deleteMessage(ctx.chatId, message.message_id);
+      return ctx.api.editMessageText(ctx.chatId, message_id, "not found");
+    } else {
+      return ctx.reply("not found");
     }
+  }
 
-    await ctx.replyWithMediaGroup(mediaGroup);
+  const mediaGroup: InputMediaPhoto[] = pageData.screenshots.map(
+    (screenshot) => ({
+      type: "photo",
+      media: screenshot,
+    }),
+  );
 
-    pageData.title &&
-      ctx.reply(
-        `
+  const linksLabel = pageData.links
+    .map((link) => `${link.quality}`)
+    .join(" | ");
+
+  if (ctx.chatId) {
+    await ctx.api.deleteMessage(ctx.chatId, message_id);
+  }
+
+  await ctx.replyWithMediaGroup(mediaGroup);
+
+  pageData.title &&
+    ctx.reply(
+      `
         ${pageData.title} \n\n
         ${linksLabel}
         `,
-        {
-          reply_markup: downloadMenu,
-        },
-      );
-  },
-);
+      {
+        reply_markup: downloadMenu,
+      },
+    );
+}
