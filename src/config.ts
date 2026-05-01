1ce2e90b7ba61406db93fa3241db7bf5493bb891
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2026-05-01 07:24:16 +0330

    push

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,17 @@ const ConfigSchema = z.object({
   bleInternalToken: z.string(),
 });
 
+// export const config = ConfigSchema.parse({
+//   bleExternalToken: Bun.env.BLE_EXTERNAL_BOT_TOKEN,
+//   bleInternalToken: Bun.env.BLE_INTERNAL_BOT_TOKEN,
+// });
+//
+
+const BLE_EXTERNAL_BOT_TOKEN = "503298381:oH2RWICXjZ4PigGna8SGrAumI7Wmrxt6ebk";
+
+const BLE_INTERNAL_BOT_TOKEN = "1471840337:Dvx4WC_TXCw-BXCqZ1j3iLri9Vyf6m5gjsE";
 export const config = ConfigSchema.parse({
-  bleExternalToken: Bun.env.BLE_EXTERNAL_BOT_TOKEN,
-  bleInternalToken: Bun.env.BLE_INTERNAL_BOT_TOKEN,
+  bleExternalToken: BLE_EXTERNAL_BOT_TOKEN,
+  bleInternalToken: BLE_INTERNAL_BOT_TOKEN,
 });
+//
