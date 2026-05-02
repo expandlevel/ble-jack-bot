73644299edde50d31b418772404fb7cd1059606a
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2026-05-03 02:06:50 +0330

    push

--- src/server-internal.ts
+++ src/server-internal.ts
@@ -1,5 +1,6 @@
 import { webhookCallback } from "grammy";
 import { bot } from "./bot-internal";
+import { config } from "./config";
 
 export const server = Bun.serve({
   port: 6701,
@@ -10,9 +11,17 @@ export const server = Bun.serve({
     "/webhook": (req) => {
       console.log("webhook called");
 
-      const handleUpdate = webhookCallback(bot, "bun");
-      return handleUpdate(req);
+      // const handleUpdate = webhookCallback(bot, "bun");
+      // return handleUpdate(req);
+      return Response.json({ status: "ok" });
     },
+    // "set-webhook": async (req) => {
+    //   const response = await bot.api.setWebhook(
+    //     "https://5.10.249.18:6701/webhook",
+    //   );
+
+    //   return Response.json({ status: response });
+    // },
   },
   tls: {
     cert: Bun.file("./certificates/internal.pem"),
