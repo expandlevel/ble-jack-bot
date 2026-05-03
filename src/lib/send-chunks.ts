2efbb7a8876de9b3d92b017078d17689765ad5d7
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2026-05-04 01:28:28 +0330

    push

--- src/lib/send-chunks.ts
+++ src/lib/send-chunks.ts
@@ -5,14 +5,18 @@ import { readdir } from "node:fs/promises";
 export async function sendChunks(ctx: MyContext) {
   const chunksList = await readdir("./tmp_download/parts/");
 
-  ctx.reply(`start sending chunks: ${chunksList}`);
+  const filteredChunksList = chunksList.filter(
+    (fileName) => fileName !== ".gitkeep",
+  );
+
+  await ctx.reply(`start sending chunks: ${filteredChunksList}`);
 
   const messageIds: string[] = [];
 
-  for (const chunkName of chunksList) {
-    const message = await ctx.replyWithDocument(
-      `https://104.252.77.32:88/tmp_download/parts/${chunkName}`,
-    );
+  for (const chunkName of filteredChunksList) {
+    const documentUrl = `https://104.252.77.32:88/tmp_download/parts/${chunkName}`;
+    await ctx.reply(`document:: ${documentUrl}`);
+    const message = await ctx.replyWithDocument(documentUrl);
 
     messageIds.push(message.document.file_id);
   }
